c1428e183b47d3318987167f93d056c38238a92e
Joseph Owonvwon <joseph.owonwo@gmail.com>
2026-08-08 02:53:16 +0100

    chore: formatted react files and added normalization scripts

--- providers/react-query.tsx
+++ providers/react-query.tsx
@@ -3,12 +3,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const queryClient = new QueryClient();
+export type QueryProviderProps = { children: React.ReactNode };
 
-export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+export function QueryProvider(props: QueryProviderProps) {
+  const { children } = props;
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
-};
+}
